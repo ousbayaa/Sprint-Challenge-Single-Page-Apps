aca16ed3f9c652c4db0e96356e2116b1c15d028e
Ous Bayaa <bayaaous@gmail.com>
2020-02-28 10:00:06 -0800

    added styling

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,8 +1,21 @@
 import React from "react";
+import styled from "styled-components";
 
 export default function CharacterCard(props) {
+  const Name = styled.h2`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+  `;
+
+  const Species = styled.p`
+  font-size: 19px;
+  text-align: center;
+  color: dodgerblue;
+  `;
+
   return <span key={props.person.id}>
-    <h2>{props.person.name}</h2>
-    <p>Species: {props.person.species}</p>
+    <Name>{props.person.name}</Name>
+    <Species>Species: {props.person.species}</Species>
   </span>;
 }
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,17 +1,25 @@
 import React, {useState} from "react";
 import CharacterList from "./CharacterList";
 import SearchForm from "./SearchForm";
+import styled from "styled-components";
 
 export default function WelcomePage() {
   const [searchTerm, setSearchTerm] = useState("");
 
+  const Image = styled.img`
+  border: 1px dashed crimson;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 40%;
+  `;
 
   return (
     <section className="welcome-page">
       <SearchForm setState={setSearchTerm} state={searchTerm}/>
       <header>
         <h1>Welcome to the ultimate fan site!</h1>
-        <img
+        <Image
           className="main-img"
           src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
           alt="rick"
